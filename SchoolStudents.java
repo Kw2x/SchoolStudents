8474bf88fdccdd1de5bbe89ad3153f5d0709daa2
Kw2x <bhen2014@gmail.com>
2024-02-05 15:14:42 -0500

    Added some methods to clean up the redundancy.

--- SchoolStudents.java
+++ SchoolStudents.java
@@ -1,51 +1,51 @@
 import java.util.Scanner; // importing Scanner Class
+ public class SchoolStudents {
+     public static void main(String[] args) {
+         // local variables
+         String userName;
+         int num1 = 4; // hard coded the int
+         int num2 = 9; // hard coded the int
+         int answer; // keep  this dynamic to check against
 
- class SchoolStudents {
-    public static void main(String[] args)
-    {
-        // local variables
-        String userName;
-        int num1 = 4; // hard coded the int
-        int num2 = 9; // hard coded the int
-        int answer; // keep  this dynamic to check against
+         //Hello Message and getting User Input
+         Scanner un = new Scanner(System.in); // Scanner to hold userName
+         Scanner an = new Scanner(System.in); // Scanner to hold the answer
+         System.out.println("Hello, please enter a name");
+         userName = un.nextLine();
+         System.out.println("Welcome to school " + userName);
 
-        Scanner un = new Scanner(System.in); // Scanner to hold userName
-        Scanner an = new Scanner(System.in); // Scanner to hold the answer
-        System.out.println("Hello, please enter a name");
-        userName = un.nextLine();
-        System.out.println("Welcome to school " + userName);
+         // Question 1
+         printQuestion (num1, "+", num2);
+         int userAnswer = an.nextInt();
+         int correctAnswer = num1 + num2;
+         answerVerification(userAnswer, correctAnswer);
 
+         // Question 2
+         printQuestion (num1, "-", num2);
+         userAnswer = an.nextInt();
+         correctAnswer = num1 - num2;
+         answerVerification(userAnswer, correctAnswer);
 
-        // Question 1
-        String question = ("what is " +  num1 + "+" + num2 + "?");
-        System.out.println(question);
-        answer = an.nextInt();
-        System.out.println("The answer  is " + (num1 + num2)); // calculation
-        System.out.println("You answered " + answer);// validation
+         // Question 3
+         printQuestion (num1, "*", num2);
+         userAnswer = an.nextInt();
+         correctAnswer = num1 * num2;
+         answerVerification(userAnswer, correctAnswer);
 
+         // Question 4
+         printQuestion (num1, "/", num2);
+         userAnswer = an.nextInt();
+         correctAnswer = num1 / num2;
+         answerVerification(userAnswer, correctAnswer);
+         un.close();
+     }
+     public static void printQuestion ( int num1, String op , int num2){
+         String question = "What is " + num1 + op + num2 + "?";
+         System.out.println(question);
+     }
+     public static void answerVerification ( int userAnswer, int correctAnswer){
+         System.out.println("The answer  is " + correctAnswer);
+         System.out.println("You answered " + userAnswer);
+     }
 
-        // Question 2
-        question = ("what is " +  num1 + "-" + num2 + "?");
-        System.out.println(question);
-        answer = an.nextInt();
-        System.out.println("The answer  is " + (num1 - num2));
-        System.out.println("You answered " + answer);
-
-
-        // Question 3
-        question = ("what is " +  num1 + "*" + num2 + "?");
-        System.out.println(question);
-        answer = an.nextInt();
-        System.out.println("The answer  is " + (num1 * num2));
-        System.out.println("You answered " + answer);
-
-
-        // Question 4
-        question = ("what is " +  num1 + "/" + num2 + "?");
-        System.out.println(question);
-        answer = an.nextInt();
-        System.out.println("The answer  is " + (num1 / num2));
-        System.out.println("You answered " + answer);
-        un.close();
-    }
-}
+ }
