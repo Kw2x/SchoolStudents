7b8c90c285bef3bf30932aa72a08fdfe291b0d98
Kw2x <bhen2014@gmail.com>
2024-02-05 19:23:26 -0500

    Created boolean/equality checks, if statements and score counter.

--- SchoolStudents.java
+++ SchoolStudents.java
@@ -5,12 +5,15 @@ import java.util.Scanner; // importing Scanner Class
          // local variables
          String userName;
 
-         //Random numbers
+         // Random numbers
          Random random = new Random();
          int num1; // dynamic to take randoms
          int num2; // dynamic to take randoms
          num1 = random.nextInt(10) + 1;
          num2 = random.nextInt(10) + 1;
+
+         // Score Counter
+         int score = 0;
 
          //Hello Message and getting User Input
          Scanner un = new Scanner(System.in); // Scanner to hold userName
@@ -23,25 +26,55 @@ import java.util.Scanner; // importing Scanner Class
          printQuestion (num1, "+", num2);
          int userAnswer = an.nextInt();
          int correctAnswer = num1 + num2;
+         if (userAnswer == correctAnswer){
+             score = score + 1;
+             System.out.println("Your score is " + score);
+         }
+         else {
+             System.out.println("Your score is " + (score = score));
+         }
          answerVerification(userAnswer, correctAnswer);
+         System.out.println("Your score + " + score);
 
          // Question 2
          printQuestion (num1, "-", num2);
          userAnswer = an.nextInt();
          correctAnswer = num1 - num2;
+         if (userAnswer == correctAnswer){
+             score = score + 1;
+             System.out.println("Your score is " + score);
+         }
+         else {
+             System.out.println("Your score is " + (score = score));
+             }
          answerVerification(userAnswer, correctAnswer);
+         System.out.println(userAnswer == correctAnswer);
 
          // Question 3
          printQuestion (num1, "*", num2);
          userAnswer = an.nextInt();
          correctAnswer = num1 * num2;
+         if (userAnswer == correctAnswer){
+             score = score + 1;
+             System.out.println("Your score is " + score);
+         }
+         else {
+             System.out.println("Your score is " + (score = score));
+         }
          answerVerification(userAnswer, correctAnswer);
+         System.out.println(userAnswer == correctAnswer);
 
          // Question 4
          printQuestion (num1, "/", num2);
          userAnswer = an.nextInt();
          correctAnswer = num1 / num2;
+         if (userAnswer == correctAnswer){
+             score = score + 1;
+             System.out.println("Your score is " + score);
+         } else
+             System.out.println("Your score is " + (score = score));
          answerVerification(userAnswer, correctAnswer);
+         System.out.println(userAnswer == correctAnswer);
          un.close();
      }
      public static void printQuestion ( int num1, String op , int num2){
@@ -52,5 +85,6 @@ import java.util.Scanner; // importing Scanner Class
          System.out.println("The answer  is " + correctAnswer);
          System.out.println("You answered " + userAnswer);
      }
+        }
 
- }
+
