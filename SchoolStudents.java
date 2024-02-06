8bddccaf46da6227e04f8a6c71c8acadd220830f
Kw2x <bhen2014@gmail.com>
2024-02-06 13:03:10 -0500

    Created scoreCounter to clean up if statements, but it never updates

--- SchoolStudents.java
+++ SchoolStudents.java
@@ -12,9 +12,6 @@ import java.util.Scanner; // importing Scanner Class
          num1 = random.nextInt(10) + 1;
          num2 = random.nextInt(10) + 1;
 
-         // Score Counter
-         int score = 0;
-
          //Hello Message and getting User Input
          Scanner un = new Scanner(System.in); // Scanner to hold userName
          Scanner an = new Scanner(System.in); // Scanner to hold the answer
@@ -22,69 +19,59 @@ import java.util.Scanner; // importing Scanner Class
          userName = un.nextLine();
          System.out.println("Welcome to school " + userName);
 
+
          // Question 1
-         printQuestion (num1, "+", num2);
+         printQuestion(num1, "+", num2);
          int userAnswer = an.nextInt();
          int correctAnswer = num1 + num2;
-         if (userAnswer == correctAnswer){
-             score = score + 1;
-             System.out.println("Your score is " + score);
-         }
-         else {
-             System.out.println("Your score is " + (score = score));
-         }
          answerVerification(userAnswer, correctAnswer);
-         System.out.println("Your score + " + score);
+         scoreCounter(userAnswer, correctAnswer);
+         System.out.println(userAnswer == correctAnswer);
 
          // Question 2
-         printQuestion (num1, "-", num2);
+         printQuestion(num1, "-", num2);
          userAnswer = an.nextInt();
          correctAnswer = num1 - num2;
-         if (userAnswer == correctAnswer){
-             score = score + 1;
-             System.out.println("Your score is " + score);
-         }
-         else {
-             System.out.println("Your score is " + (score = score));
-             }
+         scoreCounter(userAnswer, correctAnswer);
          answerVerification(userAnswer, correctAnswer);
          System.out.println(userAnswer == correctAnswer);
 
+
          // Question 3
-         printQuestion (num1, "*", num2);
+         printQuestion(num1, "*", num2);
          userAnswer = an.nextInt();
          correctAnswer = num1 * num2;
-         if (userAnswer == correctAnswer){
-             score = score + 1;
-             System.out.println("Your score is " + score);
-         }
-         else {
-             System.out.println("Your score is " + (score = score));
-         }
+         scoreCounter(userAnswer, correctAnswer);
          answerVerification(userAnswer, correctAnswer);
          System.out.println(userAnswer == correctAnswer);
 
          // Question 4
-         printQuestion (num1, "/", num2);
+         printQuestion(num1, "/", num2);
          userAnswer = an.nextInt();
          correctAnswer = num1 / num2;
-         if (userAnswer == correctAnswer){
-             score = score + 1;
-             System.out.println("Your score is " + score);
-         } else
-             System.out.println("Your score is " + (score = score));
+         scoreCounter(userAnswer, correctAnswer);
          answerVerification(userAnswer, correctAnswer);
          System.out.println(userAnswer == correctAnswer);
          un.close();
      }
-     public static void printQuestion ( int num1, String op , int num2){
+
+     public static void printQuestion(int num1, String op, int num2) {
          String question = "What is " + num1 + op + num2 + "?";
          System.out.println(question);
      }
-     public static void answerVerification ( int userAnswer, int correctAnswer){
+     public static void answerVerification(int userAnswer, int correctAnswer) {
          System.out.println("The answer  is " + correctAnswer);
          System.out.println("You answered " + userAnswer);
      }
-        }
+     private static void scoreCounter(int userAnswer, int correctAnswer) {
+         int score = 0;
+         if (userAnswer == correctAnswer) {
+             score = score + 1;
+             System.out.println("Your score is " + score);
+         } else {
+             System.out.println("Your score is " + (score = score));
+         }
+     }
+ }
 
 
